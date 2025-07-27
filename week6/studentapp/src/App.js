3e183484de71c8b0b6670f7ff41f15bf9eaad9da
Ayush <kr.ayush0603@gmail.com>
2025-07-27 23:12:40 +0530

    Add files via upload

---
+++ week6/studentapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
+
